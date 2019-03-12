f7bacc2bbf804dd4a722bdfbd8e1eb1cf5a934c1
Deepak Thakur <deepak4218@yahoo.com>
2019-03-12 16:39:48 +0530

    Added faceid / touchid using local-authentication framework

--- ExtensionsPOC/ViewController.swift
+++ ExtensionsPOC/ViewController.swift
@@ -8,28 +8,44 @@
 
 import UIKit
 import Foundation
-
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var successLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //self.extensionContext?.widgetLargestAvailableDisplayMode = .expanded
     }
     
-//    func widgetActiveDisplayModeDidChange(_ activeDisplayMode: NCWidgetDisplayMode, withMaximumSize maxSize: CGSize)
-//    {
-//        if activeDisplayMode == .expanded
-//        {
-//            preferredContentSize = CGSize(width: 0.0, height: 300.0)
-//        }
-//        else
-//        {
-//            preferredContentSize = maxSize
-//        }
-//    }
+    @IBAction func touchOrFaceID(_ sender: UIButton) {
+        let myContext = LAContext()
+        let myLocalizedReasonString = "Biometric Authntication testing !! "
+        
+        var authError: NSError?
+        if #available(iOS 8.0, macOS 10.12.1, *) {
+            if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+                myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                    
+                    DispatchQueue.main.async {
+                        if success {
+                            // User authenticated successfully, take appropriate action
+                            self.successLabel.text = "Awesome!!... User authenticated successfully"
+                        } else {
+                            // User did not authenticate successfully, look at error and take appropriate action
+                            self.successLabel.text = "Sorry!!... User did not authenticate successfully"
+                        }
+                    }
+                }
+            } else {
+                // Could not evaluate policy; look at authError and present an appropriate message to user
+                successLabel.text = "Sorry!!.. Could not evaluate policy."
+            }
+        } else {
+            // Fallback on earlier versions
+            successLabel.text = "Ooops!!.. This feature is not supported."
+        }
+    }
 
     
 }
